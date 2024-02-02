ca7b3fb9c60fdb7d83b9c32a8f558c9aa119930d
Danila Tikhonov <danila@jiaxyga.com>
2024-02-02 20:27:13 +0300

    include: drm_mipi_dsi: add support for 10bit panels
    Signed-off-by: Danila Tikhonov <danila@jiaxyga.com>
    (cherry picked from commit bc6f272d14d6cd48f5c7d0b975bf80943aad1ff4)

--- include/drm/drm_mipi_dsi.h
+++ include/drm/drm_mipi_dsi.h
@@ -140,6 +140,7 @@ struct mipi_dsi_host *of_find_mipi_dsi_host_by_node(struct device_node *node);
 #define MIPI_DSI_HS_PKT_END_ALIGNED	BIT(12)
 
 enum mipi_dsi_pixel_format {
+	MIPI_DSI_FMT_RGB101010,
 	MIPI_DSI_FMT_RGB888,
 	MIPI_DSI_FMT_RGB666,
 	MIPI_DSI_FMT_RGB666_PACKED,
